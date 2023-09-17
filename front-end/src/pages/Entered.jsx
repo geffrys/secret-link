e67a0c3fc80aad76ba0a3c1f9b80e2f3f829ab45
geffrys <74202340+geffrys@users.noreply.github.com>
2023-09-17 12:31:48 -0500

    Entered page base created
    html base created, pending to add context and scripts

--- front-end/src/pages/Entered.jsx
+++ front-end/src/pages/Entered.jsx
@@ -1,34 +1,123 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import "../css/Entered.css";
 
 
 function EnteredClient() {
-    const navigate = useNavigate();
-
-
-
     return (
         <div className="entered_container">
+
             <div className="entered_main_content">
-                <h1>UserName</h1>
-                <div>
-                    <h1>
-                        current reservations
+                {/* client name from context */}
+                <div className="entered_user_title_container">
+                    <h1 className="entered_user_title">
+                        <b>
+                            UserName
+                        </b>
                     </h1>
+                </div>
+
+
+                {/* current reservations */}
+
+                <div className="section_reservations">
+
+                    <div className="section_header">
+                        <div className="section_title_container">
+                            <h1>
+                                current reservations
+                            </h1>
+                        </div>
+                        <div className="section_actions">
+                            <button className="new_reservation_btn">
+                                New reservation
+                            </button>
+                            <div>
+                                <input type="text" placeholder="search" />
+                            </div>
+                        </div>
+                    </div>
+
                     <div>
-                        <button>
-                            New reservation
-                        </button>
-                        <input type="text" placeholder="search" />
+                        <div className="section_content">
+                            <table>
+
+                                <thead>
+                                    <tr>
+                                        <th>Status</th>
+                                        <th>Initial Date</th>
+                                        <th>Payment</th>
+                                        <th># Reservation</th>
+                                        <th># people</th>
+                                        <th>Headquarter</th>
+                                        <th>Total cost</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <br />
+                {/* previous reservations */}
+
+
+                <div className="section_reservations">
+
+
+                    <div className="section_header">
+                        <div className="section_title_container">
+                            <h1>
+                                past reservations
+                            </h1>
+                        </div>
+                        <div className="section_actions">
+                            
+                            <div className="date_actions">
+                                <p>desde</p>
+                                <input type="date" />
+                                <p>hasta</p>
+                                <input type="date" />
+                            </div>
+                            <div>
+                                <input type="text" placeholder="search" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="section_content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Status</th>
+                                    <th>Initial Date</th>
+                                    <th>Payment</th>
+                                    <th># Reservation</th>
+                                    <th># people</th>
+                                    <th>Headquarter</th>
+                                    <th>Total cost</th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
 
                 </div>
+
+
             </div>
-            <div className="entered_metada">
-                <b>Con nosotros desde:</b>
-                <p></p>
+
+            <div className="entered_metadata">
+                <h1>
+                    <b>Con nosotros desde:</b>
+                </h1>
+                <p>x fecha</p>
             </div>
+
+
         </div>
     )
 }
