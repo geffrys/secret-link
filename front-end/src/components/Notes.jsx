b2a488b692a4f2289136b75ca39a0df47d8584c9
sandres07 <sergio_bula82211@elpoli.edu.co>
2023-10-04 13:56:20 -0500

    the notes component was edited
    The notes component and its css were edited

--- front-end/src/components/Notes.jsx
+++ front-end/src/components/Notes.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import '../css/Notes.css';
+import { useState } from 'react';
+import '../css/Notes.css'; // Importa el archivo CSS para estilizar el menú
 
 function Notes() {
-    return (
-        <section className="notes">
-            <label>Notes</label>
-            <div>
-                <input className='escribir' type="text" placeholder='escribir...' />
-            </div>
-        </section>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNotes = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="notes-container">
+      <button className="notes-button" onClick={toggleNotes}>
+        Notes
+      </button>
+      {isOpen && (
+        <ul className="notes-content">
+          <input className='escribir' type="text" placeholder='escribir...' />
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Notes;
